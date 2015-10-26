1abd555f64eb9ee9aa68a2fb15939319183fcc6a
Paul Götze <paul.christoph.goetze@gmail.com>
2015-10-26 20:32:05 +0100

    First refactoring

--- lib/figures.rb
+++ lib/figures.rb
@@ -2,59 +2,91 @@ require_relative "figures/version"
 
 module Figures
 
+  ONE     = 0.freeze
+  TEN     = 1.freeze
+  HUNDRED = 2.freeze
+
+  GERMAN_WORDS = [
+    [ :und, :ein, :zwei, :drei, :vier, :fünf, :sechs, :sieben, :acht, :neun ],
+    [ :eine, :zehn, :zwanzig, :dreißig, :vierzig, :fünfzig, :sechzig, :siebzig, :achzig, :neunzig ],
+    [
+      :hundert, :tausend, :million, :milliarden, :billion, :billiarden, :trillion, :trilliarden,
+      :quadrillionen, :quadrilliarden, :quintillion, :sextillion, :sextilliarden
+    ]
+  ].freeze
+
   def self.parse(number, language_code = :de)
-    number_triples = number.to_s.reverse.scan(/.{1,3}/).map{ |triple| triple.reverse.to_i }
-    number_triples.map.with_index { |part, index| german_words_for(part.to_i, index) }.reverse.join('').gsub(/^und/, '')
+    number_triples = number.to_s.reverse.scan(/.{1,3}/).map do |triple|
+      triple.reverse.to_i
+    end
+
+    word_parts = number_triples.map.with_index do |part, index|
+      german_words_for(part.to_i, index)
+    end
+
+    word_parts.reverse.join('').gsub(/^und/, '')
   end
 
-  ONE = 0
-  TEN = 1
-  HUNDRED = 2
-
   def self.german_words_for(number, triple_index)
-    return 'null' if number == 0
-    return 'eins' if number == 1 && triple_index == 0
+    return 'null'       if number == 0
+    return 'eins'       if number == 1 && triple_index == 0
     return 'minus eins' if number == -1 && triple_index == 0
-
-    words = [
-        [ :und, :ein, :zwei, :drei, :vier, :fünf, :sechs, :sieben, :acht, :neun ],
-        [ :eine, :zehn, :zwanzig, :dreißig, :vierzig, :fünfzig, :sechzig, :siebzig, :achzig, :neunzig ],
-        [
-          :hundert, :tausend, :million, :milliarden, :billion, :billiarden, :trillion, :trilliarden,
-          :quadrillionen, :quadrilliarden, :quintillion, :sextillion, :sextilliarden
-        ]
-      ]
 
     temp_number = number.abs
     number_word = ""
-    temp_tens = ""
+    temp_tens   = ""
 
-    while  temp_number > 0
+    while temp_number > 0
       decimal_power = Math.log10(temp_number).floor
       num = temp_number - (temp_number % (10 ** decimal_power))
       index = num / (10 ** decimal_power)
-      copula = ((index > 1) ? words[0][0].to_s : "")
-      leading_single = (triple_index >= 2 && index == 1) ? words[1][0].to_s : words[0][index].to_s
+      copula = ((index > 1) ? GERMAN_WORDS[0][0].to_s : "")
+      leading_single = (triple_index >= 2 && index == 1) ? GERMAN_WORDS[1][0].to_s : GERMAN_WORDS[0][index].to_s
 
-      number_word << words[0][index].to_s << words[2][0].to_s if decimal_power == HUNDRED
-      number_word << words[0][index].to_s if decimal_power == ONE && !temp_tens.empty?
-      number_word << copula << leading_single if decimal_power == ONE && temp_tens.empty?
-      temp_tens << copula << words[1][index].to_s if decimal_power == TEN
+      if decimal_power == HUNDRED
+        number_word << GERMAN_WORDS[0][index].to_s << GERMAN_WORDS[2][0].to_s
+      end
+
+      if decimal_power == ONE && !temp_tens.empty?
+        number_word << GERMAN_WORDS[0][index].to_s
+      end
+
+      if decimal_power == ONE && temp_tens.empty?
+        number_word << copula << leading_single
+      end
+
+      if decimal_power == TEN
+        temp_tens << copula << GERMAN_WORDS[1][index].to_s
+      end
 
       temp_number = temp_number - num
     end
 
     number_word << temp_tens
-    number_word << words[2][triple_index].to_s if triple_index > 0
-    number_word = number_word.gsub('einzehn', 'elf')
-    number_word = number_word.gsub('zweizehn', 'zwölf')
-    number_word = number_word.gsub('sechszehn', 'sechzehn')
-    number_word = number_word.gsub('siebenzehn', 'siebzehn')
-    number_word = number_word.gsub(/^(und|null)/, '')
-    number_word = "minus #{number_word}" if number < 0
+
+    if triple_index > 0
+      number_word << GERMAN_WORDS[2][triple_index].to_s
+    end
+
+    number_word = handle_exceptions(number_word)
+    number_word = remove_wrong_leadings(number_word)
+
+    if number < 0
+      number_word = "minus #{number_word}"
+    end
+
     number_word
   end
 
+  def self.handle_exceptions(word)
+    word = word.gsub('einzehn', 'elf')
+    word = word.gsub('zweizehn', 'zwölf')
+    word = word.gsub('sechszehn', 'sechzehn')
+    word = word.gsub('siebenzehn', 'siebzehn')
   end
 
+  def self.remove_wrong_leadings(word)
+    word.gsub(/^(und|null)/, '')
+  end
 
+end
