4a534c68903059bf762257cd863337e863ac0571
Jan Lelis <mail@janlelis.de>
2015-10-26 20:41:52 +0100

    Extract code to German class

--- lib/figures.rb
+++ lib/figures.rb
@@ -1,92 +1,9 @@
 require_relative "figures/version"
 
+require_relative "figures/german"
+
 module Figures
-
-  ONE     = 0.freeze
-  TEN     = 1.freeze
-  HUNDRED = 2.freeze
-
-  GERMAN_WORDS = [
-    [ :und, :ein, :zwei, :drei, :vier, :fünf, :sechs, :sieben, :acht, :neun ],
-    [ :eine, :zehn, :zwanzig, :dreißig, :vierzig, :fünfzig, :sechzig, :siebzig, :achzig, :neunzig ],
-    [
-      :hundert, :tausend, :million, :milliarden, :billion, :billiarden, :trillion, :trilliarden,
-      :quadrillionen, :quadrilliarden, :quintillion, :sextillion, :sextilliarden
-    ]
-  ].freeze
-
   def self.parse(number, language_code = :de)
-    number_triples = number.to_s.reverse.scan(/.{1,3}/).map do |triple|
-      triple.reverse.to_i
-    end
-
-    word_parts = number_triples.map.with_index do |part, index|
-      german_words_for(part.to_i, index)
-    end
-
-    word_parts.reverse.join('').gsub(/^und/, '')
+    Figures::German.new(number.to_s).parse
   end
-
-  def self.german_words_for(number, triple_index)
-    return 'null'       if number == 0
-    return 'eins'       if number == 1 && triple_index == 0
-    return 'minus eins' if number == -1 && triple_index == 0
-
-    temp_number = number.abs
-    number_word = ""
-    temp_tens   = ""
-
-    while temp_number > 0
-      decimal_power = Math.log10(temp_number).floor
-      num = temp_number - (temp_number % (10 ** decimal_power))
-      index = num / (10 ** decimal_power)
-      copula = ((index > 1) ? GERMAN_WORDS[0][0].to_s : "")
-      leading_single = (triple_index >= 2 && index == 1) ? GERMAN_WORDS[1][0].to_s : GERMAN_WORDS[0][index].to_s
-
-      if decimal_power == HUNDRED
-        number_word << GERMAN_WORDS[0][index].to_s << GERMAN_WORDS[2][0].to_s
-      end
-
-      if decimal_power == ONE && !temp_tens.empty?
-        number_word << GERMAN_WORDS[0][index].to_s
-      end
-
-      if decimal_power == ONE && temp_tens.empty?
-        number_word << copula << leading_single
-      end
-
-      if decimal_power == TEN
-        temp_tens << copula << GERMAN_WORDS[1][index].to_s
-      end
-
-      temp_number = temp_number - num
-    end
-
-    number_word << temp_tens
-
-    if triple_index > 0
-      number_word << GERMAN_WORDS[2][triple_index].to_s
-    end
-
-    number_word = handle_exceptions(number_word)
-    number_word = remove_wrong_leadings(number_word)
-
-    if number < 0
-      number_word = "minus #{number_word}"
-    end
-
-    number_word
-  end
-
-  def self.handle_exceptions(word)
-    word = word.gsub('einzehn', 'elf')
-    word = word.gsub('zweizehn', 'zwölf')
-    word = word.gsub('sechszehn', 'sechzehn')
-    word = word.gsub('siebenzehn', 'siebzehn')
-  end
-
-  def self.remove_wrong_leadings(word)
-    word.gsub(/^(und|null)/, '')
-  end
-
 end
