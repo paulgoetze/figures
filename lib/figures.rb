307dc5b3c93a98aa487a2ca20e1677d97715b90c
Paul Götze <paul.christoph.goetze@gmail.com>
2015-10-28 02:11:32 +0100

    Implement basic systematics of number building

--- lib/figures.rb
+++ lib/figures.rb
@@ -1,9 +1,9 @@
-require_relative "figures/version"
-
-require_relative "figures/german"
+require "figures/version"
+require "figures/german"
 
 module Figures
-  def self.write(number, language_code = :de)
-    Figures::German.new(number.to_i).write
+
+  def self.parse(number, language_code = :de)
+    Figures::German.new(number).parse
   end
 end
--- lib/figures/german.rb
+++ lib/figures/german.rb
@@ -1,139 +1,133 @@
 module Figures
   class German
-    WORDS = {
-      copula: "und",
-      zero: "null",
-      single: "eins",
-      one: "eine",
-      first10: [""] + %w[
-        ein
-        zwei
-        drei
-        vier
-        fünf
-        sechs
-        sieben
-        acht
-        neun
-      ],
-      second10: %w[
-        zehn
-        elf
-        zwölf
-        dreizehn
-        vierzehn
-        fünfzehn
-        sechzehn
-        siebzehn
-        achtzehn
-        neunzehn
-      ],
-      tens: %w[
-        zwanzig
-        dreißig
-        vierzig
-        fünfzig
-        sechzig
-        siebzig
-        achzig
-        neunzig
-      ],
-      hundred: 'hundert',
-      thousand: 'tausend',
-      huge: %w[
-        Million
-        Milliarde
-        Billion
-        Billiarde
-        Trillion
-        Trilliarde
-        Quadrillion
-        Quadrilliarde
-        Quintillion
-        Quintilliarde
-        Sextillion
-        Sextilliarde
-        Septillion
-        Septilliarde
-        Oktillion
-        Oktilliarde
-        Oktillion
-        Oktilliarde
-        Nonillion
-        Nonilliarde
-        Dezillion
-        Dezilliarde
-        Undezillion
-        Undezilliarde
-        Dodezillion
-        Dodezilliarde
-        Tredezillion
-        Tredezilliarde
-        Quattuordezillion
-        Quattuordezilliarde
-        Quindezillion
-        Quindezilliarde
-        Sedezillion
-        Sedezilliarde
-      ]
-    }
+
+    UNITS   = %w{ eins zwei drei vier fünf sechs sieben acht neun }.freeze
+
+    PREFIXES = {
+      units:          %w{ tausend mi bi tri quadri quinti sexti septi okti noni },
+      union_units:    %w{ un duo tre quattuor quinqua se septe okto nove },
+      union_tens:     %{ dezi viginti triginta quadraginta quinquaginta sexaginta septuaginta oktoginta nonaginta },
+      union_hundreds: %w{ zenti duzenti trezenti quadringenti quingenti seszenti septingenti oktingenti nongenti }
+    }.freeze
+
+    EXCEPTIONS = {
+      /^eins(hundert|tausend)/ => 'ein\1',
+      /^eins\s/    => 'eine ',
+      'einszehn'   => 'elf',
+      'zweizehn'   => 'zwölf',
+      'sechszehn'  => 'sechzehn',
+      'siebenzehn' => 'siebzehn',
+      'zweizig'    => 'zwanzig',
+      'dreizig'    => 'dreißig',
+      'sechszig'   => 'sechzig',
+      'siebenzig'  => 'siebzig'
+    }.freeze
+
+    attr_reader :number
 
     def initialize(number)
       @number = number.to_i
     end
 
-    def write
-      return WORDS[:zero] if @number == 0
-      absolute_number_as_string = @number.abs.to_s
-      return "Rießig" if absolute_number_as_string.size > 99
+    def parse
+      return 'null' if number == 0
 
-      result = absolute_number_as_string.reverse.scan(/.{1,3}/).map.with_index{ |triple, index|
-        write_triple(triple.reverse, index)
-      }.reverse.join.strip
+      triples = split_into_reverse_triples(number)
 
-      @number < 0 ? "minus #{result}" : result
+      word = triples.each_with_index.reduce('') do |result, (triple, index)|
+        triple_word = triple_to_word(triple, index)
+        result.prepend(triple_word)
+      end
+
+      number < 0 ? "minus #{word}" : word
     end
 
-    def write_triple(triple, triple_index)
-      hundred, ten, one = triple.match(/\A(\d)??(\d)??(\d)\z/).captures.map(&:to_i)
-      result = ""
+    private
 
-      is_empty    = hundred == 0 && ten == 0 && one == 0
-      is_thousand = !is_empty && triple_index == 1
-
-      if hundred && hundred != 0
-        result << WORDS[:first10][hundred] + WORDS[:hundred]
-      end
-
-      if !ten
-        result << WORDS[:first10][one]
-      elsif ten == 0
-        if !is_thousand && hundred && one == 1
-          if triple_index > 1
-            result << WORDS[:one]
-          else
-            result << WORDS[:single]
-          end
-        else
-          result << WORDS[:first10][one]
-        end
-      elsif ten == 1
-        result << WORDS[:second10][one]
-      else
-        result << WORDS[:first10][one]
-        result << WORDS[:copula] unless one == 0
-        result << WORDS[:tens][ten - 2]
-      end
-
-      if is_thousand
-        result << WORDS[:thousand]
-      elsif triple_index > 1 && !is_empty
-        huge = WORDS[:huge][triple_index - 2].dup
-        huge[/e?$/] = "en" unless one == 1
-        result << " #{huge} "
-      end
-
-      result
+    def triples_count
+      @triples_count ||= split_into_reverse_triples(number).count
     end
+
+    def split_into_reverse_triples(number)
+      @reverse_triples ||= number.abs.to_s.reverse.scan(/.{1,3}/).map(&:reverse)
+    end
+
+    def triple_to_word(triple, triple_index)
+      hundred_digit, ten_digit, unit_digit = split_triple(triple)
+
+      word = [
+        hundred(hundred_digit),
+        unit(unit_digit),
+        copula(unit_digit, ten_digit),
+        ten(ten_digit)
+      ].join
+
+      word = append_exponent_identifier(word, triple_index)
+      cleanup_exceptions(word)
+    end
+
+    # splits up a triple into hundreds, tens and unit position
+    def split_triple(triple)
+      triple.match(/\A(\d)??(\d)??(\d)\z/).captures.map(&:to_i)
+    end
+
+    # returns the word for the given unit number
+    def unit(digit)
+      return '' if digit.zero?
+      UNITS[digit - 1]
+    end
+
+    # returns the copula between unit position and tens
+    def copula(unit_digit, ten_digit)
+      'und' if ten_digit > 1 && !unit_digit.zero?
+    end
+
+    # returns the word for the given tens digit
+    def ten(digit)
+      case digit
+      when 0 then ''
+      when 1 then 'zehn'
+      else unit(digit) + 'zig'
+      end
+    end
+
+    # returns the word for the given hundreds number
+    def hundred(digit)
+      case digit
+      when 0 then ''
+      else unit(digit) + 'hundert'
+      end
+    end
+
+    # adds the exponent word to the triple word
+    # e.g. tausend for the second triple   (index = 1)
+    #      Million for the third triple    (index = 2)
+    #      Milliarde for the fourth triple (index = 3)
+    #
+    #      indexes => PREFIXES index
+    #      2,3 => 1; 4,5 => 2; 6,7 => 3; ... : floored division by 2
+    # etc.
+    def append_exponent_identifier(word, index)
+      return word if word.empty? || index.zero? || triples_count == 1
+
+      if index == 1
+        word + PREFIXES[:units][0]
+      elsif index.even?
+        word + ' ' + (PREFIXES[:units][index / 2] + 'llion ').capitalize.strip
+      elsif index.odd?
+        plural_form = (word =~ /^eins/) ? '' : 'n'
+        word + ' ' + (PREFIXES[:units][index / 2] + "lliarde#{plural_form} ").capitalize.strip
+      end
+    end
+
+    # replaces all exceptions in the number word
+    def cleanup_exceptions(word)
+      EXCEPTIONS.each do |exception, replacement|
+        word.sub!(exception, replacement)
+      end
+
+      word
+    end
+
   end
 end
